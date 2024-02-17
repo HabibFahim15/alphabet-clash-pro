e2e67844f59ca91b9558410333315448fe3eecb3
HabibFahim15 <habibfahim71@gmail.com>
2024-02-17 14:23:52 +0600

    another commit

--- script/script.js
+++ script/script.js
@@ -10,12 +10,37 @@ function play(){
   playGroundSection.classList.remove('hidden')
 } 
 */
+ function handleKeyboardKeyUpEvent(event){
+  const playerPressed = event.key;
+  console.log('player pressed', playerPressed)
+
+  // get the expected to press
+  const currentAlphabetElement = document.getElementById('current-alphabet')
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+  console.log(playerPressed, expectedAlphabet)
+
+  // check matched or not
+  if(playerPressed === expectedAlphabet){
+    console.log('you got a point')
+  }
+  else{
+    console.log('you missed a life')
+  }
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
 
 function continuGame(){
    // step 1 : generate a random alphabet
   const alphabet = getARandomAlphabet();
   console.log(alphabet)
 
+  // set randomly generated alphabet to the screen (show it)
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet;
+
+  // set background color (button)
+  setBackgroundColorById(alphabet)
 }
 
 
@@ -23,5 +48,6 @@ function play(){
   hideElementById('home-screen')
   showElementById('play-ground')
   continuGame()
+  
 }
 
